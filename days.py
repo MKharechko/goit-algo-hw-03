62d34db025888a920c11fc3ca6c3fdcf417f64d8
MKharechko <kharechko1121@gmail.com>
2025-02-10 13:11:07 +0200

    changes_days

--- days.py
+++ days.py
@@ -2,11 +2,11 @@ from datetime import datetime
 
 def get_days_from_today(date):
     try:
-        user_date = datetime.strptime(date, "%Y.%m.%d").date()
+        user_date = datetime.strptime(date, "%Y-%m-%d").date()
         current_date = datetime.today().date()
         delta_days = (user_date - current_date).days
         return delta_days
     except ValueError:
         return "Невірний формат дати. Використовуйте YYYY.MM.DD."
-print(get_days_from_today("2025.06.09"))
+print(get_days_from_today("2025-06-09"))
     

a2ee2de28c072fd7346ed7055b5e1baa813130b2
MKharechko <kharechko1121@gmail.com>
2025-02-11 21:07:05 +0200

    correction

--- rand.py
+++ rand.py
@@ -1,9 +1,15 @@
 import random
 
 def get_numbers_ticket(min: int, max: int, quantity: int):
+    
     if min >= max or quantity > (max - min + 1) or quantity <= 0:
         return []
     
+    if min < 1:
+        return []
+    if max > 1000:
+        return []    
+
     numbers = random.sample(range(min, max + 1), quantity)
     return sorted(numbers)
 
